693882baccf00f44241676fd63fa3a0280356528
ifeanyihm <iheme.ifeanyi@yahoo.com>
2022-11-16 18:36:34 +0100

    Updates

---
+++ precache-manifest.31b5dd22a679dcb48bf6088addce8b30.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "31b0a0a9e77931d84569",
+    "url": "/Mailchimp_landing_page/static/css/main.858dd76f.chunk.css"
+  },
+  {
+    "revision": "31b0a0a9e77931d84569",
+    "url": "/Mailchimp_landing_page/static/js/main.31b0a0a9.chunk.js"
+  },
+  {
+    "revision": "ab80dc7d84887c77b59e",
+    "url": "/Mailchimp_landing_page/static/css/1.0cc71d81.chunk.css"
+  },
+  {
+    "revision": "ab80dc7d84887c77b59e",
+    "url": "/Mailchimp_landing_page/static/js/1.ab80dc7d.chunk.js"
+  },
+  {
+    "revision": "20336b24f6af6d534eee",
+    "url": "/Mailchimp_landing_page/static/js/runtime~main.20336b24.js"
+  },
+  {
+    "revision": "5a0ca9f9e61770729d9e875433c200b0",
+    "url": "/Mailchimp_landing_page/static/media/angle-right.5a0ca9f9.svg"
+  },
+  {
+    "revision": "9e0472484632bb22ddb4b6b39c1d530e",
+    "url": "/Mailchimp_landing_page/static/media/mailchimp.9e047248.svg"
+  },
+  {
+    "revision": "21cbf9c6469b0b0ef1e41f8acb064fe5",
+    "url": "/Mailchimp_landing_page/static/media/Line.21cbf9c6.svg"
+  },
+  {
+    "revision": "7afae10d6c858cbb11f909ab9920e14e",
+    "url": "/Mailchimp_landing_page/static/media/Arrow2.7afae10d.svg"
+  },
+  {
+    "revision": "5462a5dcec162ed91c3bc328cb749d4b",
+    "url": "/Mailchimp_landing_page/static/media/globe.5462a5dc.svg"
+  },
+  {
+    "revision": "4c679a6c9c4c155dd7c62da2180654da",
+    "url": "/Mailchimp_landing_page/static/media/magnifying-glass-solid.4c679a6c.svg"
+  },
+  {
+    "revision": "14acfe030b5c24a567f2654369f5680e",
+    "url": "/Mailchimp_landing_page/static/media/Arrow3.14acfe03.svg"
+  },
+  {
+    "revision": "e2cd2e5e572249d76f6372ce784849aa",
+    "url": "/Mailchimp_landing_page/static/media/Arrow-up-right.e2cd2e5e.svg"
+  },
+  {
+    "revision": "20e4c7349786f8e9e1044e3d7ffbe095",
+    "url": "/Mailchimp_landing_page/static/media/square-facebook.20e4c734.svg"
+  },
+  {
+    "revision": "0eef50479787d446b9ca754dd8be5768",
+    "url": "/Mailchimp_landing_page/static/media/twitter.0eef5047.svg"
+  },
+  {
+    "revision": "e1f78f1858abdd5bec2f615f097ddc69",
+    "url": "/Mailchimp_landing_page/static/media/instagram.e1f78f18.svg"
+  },
+  {
+    "revision": "ec17a7ba7e682e8b40d47c102a25fae2",
+    "url": "/Mailchimp_landing_page/static/media/linkedin.ec17a7ba.svg"
+  },
+  {
+    "revision": "932d472673fd4ab8a456edc4c8246a16",
+    "url": "/Mailchimp_landing_page/static/media/youtube.932d4726.svg"
+  },
+  {
+    "revision": "0cff64ad724013cccfc33a9d5eac1dbe",
+    "url": "/Mailchimp_landing_page/static/media/pinterest.0cff64ad.svg"
+  },
+  {
+    "revision": "40d16fb17f17b0ced4c18c9f7fe82268",
+    "url": "/Mailchimp_landing_page/static/media/menu.40d16fb1.svg"
+  },
+  {
+    "revision": "fdf5a75ff11f401beb0213a78b762798",
+    "url": "/Mailchimp_landing_page/static/media/Arrowback.fdf5a75f.svg"
+  },
+  {
+    "revision": "de37f4827625275511a1b574e03816f5",
+    "url": "/Mailchimp_landing_page/static/media/phone-solid.de37f482.svg"
+  },
+  {
+    "revision": "0f14c0562def5e7b0490089c672d98f6",
+    "url": "/Mailchimp_landing_page/index.html"
+  }
+];
